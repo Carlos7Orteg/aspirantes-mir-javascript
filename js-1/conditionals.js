b6ac15831794aafbfc5ed85e60a037c8a7c87cc7
Carlos <marioplus87@gmail.com>
2023-05-11 15:34:34 -0500

    solucion a los ejercicios de js

---
+++ js-1/conditionals.js
@@ -0,0 +1,21 @@
+//* 4.Ejercicio: Corregir la función ya que tiene algunos errores y no está funcionando.
+
+function calcularColor(num) {
+var color;
+if (num === 1) {
+color = "black";
+} else if (num === 2) {
+color = "blanco";
+} else if (num === 3) {
+color = "azul";
+} else {
+color = "verde";
+}
+return "El color es " + color;
+}
+
+//* código de prueba
+console.log(calcularColor(1)) // "El color es negro"
+console.log(calcularColor(2)) // "El color es blanco"
+console.log(calcularColor(3)) // "El color es azul"
+console.log(calcularColor(8)) // "El color es verde"
---
+++ js-1/hello.js
@@ -0,0 +1,11 @@
+//* 1.Ejercicio: Escribir función hola
+
+function hola(nombre) {
+  return "hola " + nombre + "!";
+}
+
+
+//* código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
